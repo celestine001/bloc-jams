896788d418d51d4b3dddc0c38bcb0886663df4aa
celestine001 <celydew@gmail.com>
2017-03-27 20:52:53 -0700

    Checkpoint-9-dom-scripts: completed

--- landing.js
+++ landing.js
@@ -1,7 +1,7 @@
-var animatePoints = function() {
- 
-     var points = document.getElementsByClassName('point');
- 
+     var pointsArray = document.getElementsByClassName('point');
+   
+     var animatePoints = function(points) { 
+
      var revealFirstPoint = function() {
          points[0].style.opacity = 1;
          points[0].style.transform = "scaleX(1) translateY(0)";
@@ -27,4 +27,18 @@ var animatePoints = function() {
      revealSecondPoint();
      revealThirdPoint();
  
+    window.onload = function() {
+        /*Automatically animate the points on a tall screen where sctolling can't trigger the animation*/
+        if (window.innerHeight > 950) {
+            animatePoints(pointsArray);
+        }
+        
+        var sellingPoints = document.getElementsByName(selling-Points)[0];
+        var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+        window.addEventListener('scroll', function(event) {
+         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             aminatePoints(pointsArray);
+         }
+        });
+    }
  };
