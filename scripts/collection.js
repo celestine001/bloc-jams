6237c04503a5a9c05d82276bd3c9d7c5ec128425
celestine001 <celydew@gmail.com>
2017-09-17 21:12:35 -0700

    checkpoint-17-refactor

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,4 +1,6 @@
-var collectionItemTemplate = 
+//#1
+var buildCollectionItemTemplate = function() {
+    var template =
     '<div class= "collection-album-container column fourth">'
     + '<img src="assets/images/album_covers/01.png"/>'
     + '<div class="collection-album-info caption">'
@@ -12,14 +14,22 @@ var collectionItemTemplate =
     +  '</p>'
     + '</div>'
     + '</div>';
-window.onload = function() {
+(window).load(function) {
      // #1
-     var collectionContainer = document.getElementsByClassName('album-covers')[0];
-     // #2
+     
+    // #2
+    return $(template);
+};
      collectionContainer.innerHTML = '';
  
      // #3
-     for (var i = 0; i < 12; i++) {
-         collectionContainer.innerHTML += collectionItemTemplate;
+         var $collectionContainer = $('.album-covers');
+     //#4
+         $collectionContainer.empty();
+         
+         for (var i = 0; i < 12; i++) {
+             var $newThumbnail = buildCollectionItemTemplate();
+        //#5
+             $collectionContainer.append($newThumbnail);
      }
-   }    
+   });    
